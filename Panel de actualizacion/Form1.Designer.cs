e15fae6b544cce5413215427c9d15301cd321f72
uriel <urielsalimora@gmail.com>
2024-09-29 18:11:54 -0600

    Agregar archivos de proyecto.

---
+++ Panel de actualizacion/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Panel_de_actualizacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonActualizar = new System.Windows.Forms.Button();
+            this.buttonRegresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¿Desea actualizar la aplicación?";
+            // 
+            // buttonActualizar
+            // 
+            this.buttonActualizar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.buttonActualizar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonActualizar.Location = new System.Drawing.Point(25, 60);
+            this.buttonActualizar.Name = "buttonActualizar";
+            this.buttonActualizar.Size = new System.Drawing.Size(104, 31);
+            this.buttonActualizar.TabIndex = 1;
+            this.buttonActualizar.Text = "Confirmar";
+            this.buttonActualizar.UseVisualStyleBackColor = false;
+            this.buttonActualizar.Click += new System.EventHandler(this.buttonActualizar_Click);
+            // 
+            // buttonRegresar
+            // 
+            this.buttonRegresar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.buttonRegresar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonRegresar.Location = new System.Drawing.Point(169, 60);
+            this.buttonRegresar.Name = "buttonRegresar";
+            this.buttonRegresar.Size = new System.Drawing.Size(92, 33);
+            this.buttonRegresar.TabIndex = 2;
+            this.buttonRegresar.Text = "Regresar";
+            this.buttonRegresar.UseVisualStyleBackColor = false;
+            this.buttonRegresar.Click += new System.EventHandler(this.buttonRegresar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(298, 135);
+            this.Controls.Add(this.buttonRegresar);
+            this.Controls.Add(this.buttonActualizar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Actualizar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonActualizar;
+        private System.Windows.Forms.Button buttonRegresar;
+    }
+}
+
---
+++ Panel de actualizacion/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Panel_de_actualizacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRegresar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonActualizar_Click(object sender, EventArgs e)
+        {
+            // Obtiene la ruta del directorio donde se encuentra el ejecutable
+            string ruta = Application.StartupPath;
+
+            // Define la ruta completa de la nueva carpeta
+            string nuevaCarpeta = System.IO.Path.Combine(ruta, "prueba");
+
+            // Verifica si la carpeta ya existe
+            if (!System.IO.Directory.Exists(nuevaCarpeta))
+            {
+                // Crea la carpeta
+                System.IO.Directory.CreateDirectory(nuevaCarpeta);
+            }
+
+            // URL del repositorio
+            string repoUrl = "https://github.com/UrielSaMo/CalculadoraAreasParaActualizacion.git";
+
+            // Comando Git para clonar el repositorio
+            string gitCommand = $"git clone {repoUrl} \"{nuevaCarpeta}\"";
+
+            // Ejecutar el comando de Git
+            try
+            {
+                var process = new System.Diagnostics.Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = $"/C {gitCommand}";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                process.WaitForExit();
+                MessageBox.Show("Repositorio clonado exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al clonar el repositorio: {ex.Message}");
+            }
+        }
+    }
+}
+// actualizar el programa
+// iniciar el proyecto
+// Presionar el bon sctualizar 
+// Abrir una segunda app para realizar la actualizacion 
+// permitir actualizacion ( eliminar paquetes y descargar nuevos)
+// abri nueva aplicacion (boton actualizar eliminado)
